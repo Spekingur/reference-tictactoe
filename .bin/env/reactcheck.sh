3c16508aef832c8a846b6450b62a3f3a2d86a49a
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2016-12-12 14:29:17 +0000

    removed uneccessary script and added text to script

--- .bin/env/reactcheck.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Created by Hreidar Olafur Arnarsson
-# Checks if React is installed
---
+++ .bin/env/yarncheck.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Created by Hreidar Olafur Arnarsson
+# Checks if Yarn is installed
+
+yarn --version
