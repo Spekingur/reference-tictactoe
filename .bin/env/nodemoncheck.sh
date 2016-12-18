b4906bf217e675f325918df626bb22b20b414d4b
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2016-12-18 01:17:19 +0000

    fixed script so it looked better

--- .bin/env/nodemoncheck.sh
+++ .bin/env/nodemoncheck.sh
@@ -2,10 +2,9 @@
 # Created by Hreidar Olafur Arnarsson
 # Checks whether nodemon is installed
 
-rc = $?
-
 nodemon --version
 
+rc = $?
 if [[ $rc != 0]] ; then
   echo "Nodemon is not installed."
   exit
