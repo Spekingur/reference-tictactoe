3d5dd915b4cc478d6e17b93415b84dc473a717ab
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2016-12-18 01:24:01 +0000

    can now take in an argument for production db migration

--- runserver.sh
+++ runserver.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-set -e
+set -e                              # stops on any error
 
-sleep 10
-npm run migratedb
-node run.js
+sleep 10                            # delay for 10 seconds
+case "$1" in
+  "production")                     # input is runserver.sh production
+    npm run migratedb -- -e prod    # run database migration against production database
+    ;;
+  "")                               # input is runserver.sh
+    npm run migratedb               # run database migration against dev database
+    ;;
+esac
+
+node run.js                         # start up
 
 exit 0
