f769634d3da6ee51efaec1f677a4a631f43a3d99
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2016-12-19 00:37:29 +0000

    more fixing of production deploy script

--- deploy-production.sh
+++ deploy-production.sh
@@ -7,10 +7,10 @@
 ssh -i ../../.aws/admin-key-pair-euwest1.pem ec2-user@ec2-52-212-119-153.eu-west-1.compute.amazonaws.com docker-compose down
 
 # We then copy the .env file so the docker-compose knows what image to run
-scp -i ../../.aws/admin-key-pair-euwest1.pem ../TicTacToe_Commit/.env ec2-user@ec2-52-212-119-153.eu-west-1.compute.amazonaws.com:~/.env
+scp -i ../../.aws/admin-key-pair-euwest1.pem ../TicTacToe_commit/.env ec2-user@ec2-52-212-119-153.eu-west-1.compute.amazonaws.com:~/.env
 
 # Copy the docker-compose.yml file so that docker-compose up -d command knows what to do
-scp -i ../../.aws/admin-key-pair-euwest1.pem ../TicTacToe_Commit/docker-compose.yml ec2-user@ec2-52-212-119-153.eu-west-1.compute.amazonaws.com:~/docker-compose.yml
+scp -i ../../.aws/admin-key-pair-euwest1.pem ../TicTacToe_commit/docker-compose.yml ec2-user@ec2-52-212-119-153.eu-west-1.compute.amazonaws.com:~/docker-compose.yml
 
 # (Re)start the docker-compose instances
 ssh -i ../../.aws/admin-key-pair-euwest1.pem ec2-user@ec2-52-212-119-153.eu-west-1.compute.amazonaws.com docker-compose up -d
